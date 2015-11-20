3e2893ff1032c2eda9b9b1b84584fe0ae9257021
Joseph Misiti <joseph.misiti@gmail.com>
2015-11-20 15:33:45 -0500

    added more code

--- Makefile
+++ Makefile
@@ -4,5 +4,14 @@ crawl:
 build:
 	go build -o bin/crawler crawl_products.go
 	
+clean:
+	rm json/*
 	
-.PHONY: crawl build 
+all:
+	ls json/*.json | head -n 1 | xargs cat | jq --raw-output '.'
+	
+process:
+	ls json/*.json | xargs cat | jq --raw-output '. | ."consolidated-categories" | .categories[] \
+	 | .products[] | [.PRODUCT_ID, .SHORT_DESC, .defaultSku.HEX_VALUE_STRING, .defaultSku.formattedPrice, .defaultSku.PRODUCT_SIZE, .url ] | @csv'
+	
+.PHONY: crawl build clean
--- crawl_products.go
+++ crawl_products.go
@@ -1,55 +1,42 @@
 package main
 
 import(
+    "os"
     "fmt"
     "log"
-    
+    "regexp"
     "github.com/PuerkitoBio/goquery"
+    "github.com/nu7hatch/gouuid"
 )
 
+func check(e error) {
+    if e != nil {
+        panic(e)
+    }
+}
+
+
 func getUrls() []string {
-    // urls := []string{
-    //     "http://www.maccosmetics.com/products/13834/Products/Makeup/Eyes/Brow",
-    //     "http://www.maccosmetics.com/products/13840/Products/Makeup/Eyes/Shadow",
-    //     "http://www.maccosmetics.com/products/13835/Products/Makeup/Eyes/Eye-Palettes-Kits",
-    //     "http://www.maccosmetics.com/products/14756/Products/Makeup/Eyes/Eye-Primer",
-    //     "http://www.maccosmetics.com/products/13837/Products/Makeup/Eyes/Lash",
-    //     "http://www.maccosmetics.com/products/13838/Products/Makeup/Eyes/Liner",
-    //     "http://www.maccosmetics.com/products/13839/Products/Makeup/Eyes/Mascara",
-    //     "http://www.maccosmetics.com/products/13825/Products/Skincare/Primers",
-    //     "http://www.maccosmetics.com/products/14771/Products/Skincare/BB-CC",
-    //     "http://www.maccosmetics.com/products/13824/Products/Skincare/Moisturizers",
-    //     "http://www.maccosmetics.com/products/13825/Products/Skincare/Primers",
-    //     "http://www.maccosmetics.com/products/13826/Products/Skincare/Removers",
-    //     "http://www.maccosmetics.com/products/13827/Products/Skincare/Travel-Size",
-    //     "http://www.maccosmetics.com/products/13804/Products/Brushes-Tools/Brushes/Eye-Brushes",
-    //     "http://www.maccosmetics.com/products/13802/Products/Brushes-Tools/Brushes/All-Brushes",
-    //     "http://www.maccosmetics.com/products/13803/Products/Brushes-Tools/Brushes/Brush-Kits",
-    //     "http://www.maccosmetics.com/products/13805/Products/Brushes-Tools/Brushes/Face-Brushes",
-    //     "http://www.maccosmetics.com/products/13806/Products/Brushes-Tools/Brushes/Lip-Brushes",
-    //     "http://www.maccosmetics.com/products/13815/Products/Fragrance/Turquatic",
-    // }
-    
     urls := []string{
         "http://www.maccosmetics.com/products/13834/Products/Makeup/Eyes/Brow",
-        // "http://www.maccosmetics.com/products/13840/Products/Makeup/Eyes/Shadow",
-        // "http://www.maccosmetics.com/products/13835/Products/Makeup/Eyes/Eye-Palettes-Kits",
-        // "http://www.maccosmetics.com/products/14756/Products/Makeup/Eyes/Eye-Primer",
-        // "http://www.maccosmetics.com/products/13837/Products/Makeup/Eyes/Lash",
-        // "http://www.maccosmetics.com/products/13838/Products/Makeup/Eyes/Liner",
-        // "http://www.maccosmetics.com/products/13839/Products/Makeup/Eyes/Mascara",
-        // "http://www.maccosmetics.com/products/13825/Products/Skincare/Primers",
-        // "http://www.maccosmetics.com/products/14771/Products/Skincare/BB-CC",
-        // "http://www.maccosmetics.com/products/13824/Products/Skincare/Moisturizers",
-        // "http://www.maccosmetics.com/products/13825/Products/Skincare/Primers",
-        // "http://www.maccosmetics.com/products/13826/Products/Skincare/Removers",
-        // "http://www.maccosmetics.com/products/13827/Products/Skincare/Travel-Size",
-        // "http://www.maccosmetics.com/products/13804/Products/Brushes-Tools/Brushes/Eye-Brushes",
-        // "http://www.maccosmetics.com/products/13802/Products/Brushes-Tools/Brushes/All-Brushes",
-        // "http://www.maccosmetics.com/products/13803/Products/Brushes-Tools/Brushes/Brush-Kits",
-        // "http://www.maccosmetics.com/products/13805/Products/Brushes-Tools/Brushes/Face-Brushes",
-        // "http://www.maccosmetics.com/products/13806/Products/Brushes-Tools/Brushes/Lip-Brushes",
-        // "http://www.maccosmetics.com/products/13815/Products/Fragrance/Turquatic",
+        "http://www.maccosmetics.com/products/13840/Products/Makeup/Eyes/Shadow",
+        "http://www.maccosmetics.com/products/13835/Products/Makeup/Eyes/Eye-Palettes-Kits",
+        "http://www.maccosmetics.com/products/14756/Products/Makeup/Eyes/Eye-Primer",
+        "http://www.maccosmetics.com/products/13837/Products/Makeup/Eyes/Lash",
+        "http://www.maccosmetics.com/products/13838/Products/Makeup/Eyes/Liner",
+        "http://www.maccosmetics.com/products/13839/Products/Makeup/Eyes/Mascara",
+        "http://www.maccosmetics.com/products/13825/Products/Skincare/Primers",
+        "http://www.maccosmetics.com/products/14771/Products/Skincare/BB-CC",
+        "http://www.maccosmetics.com/products/13824/Products/Skincare/Moisturizers",
+        "http://www.maccosmetics.com/products/13825/Products/Skincare/Primers",
+        "http://www.maccosmetics.com/products/13826/Products/Skincare/Removers",
+        "http://www.maccosmetics.com/products/13827/Products/Skincare/Travel-Size",
+        "http://www.maccosmetics.com/products/13804/Products/Brushes-Tools/Brushes/Eye-Brushes",
+        "http://www.maccosmetics.com/products/13802/Products/Brushes-Tools/Brushes/All-Brushes",
+        "http://www.maccosmetics.com/products/13803/Products/Brushes-Tools/Brushes/Brush-Kits",
+        "http://www.maccosmetics.com/products/13805/Products/Brushes-Tools/Brushes/Face-Brushes",
+        "http://www.maccosmetics.com/products/13806/Products/Brushes-Tools/Brushes/Lip-Brushes",
+        "http://www.maccosmetics.com/products/13815/Products/Fragrance/Turquatic",
     }
     return urls
 }
@@ -67,6 +54,28 @@ func parseWeight(s *goquery.Selection) string {
     return s.Text()
 }
 
+func parseHtml(url string) {
+    
+    doc, err := goquery.NewDocument(url) 
+      if err != nil {
+        log.Fatal(err)
+    }
+    
+    fmt.Println("processing", url)
+    doc.Find("script").Each(func(i int, s *goquery.Selection) {
+        match, _ := regexp.MatchString("var page_data", s.Text())
+        if (match) {
+            u, err := uuid.NewV4()
+            f, err := os.Create(fmt.Sprintf("json/%s.json",u))
+            check(err)
+            f.WriteString(s.Text()[15:])
+            defer f.Close()
+            
+        }
+        
+    })  
+}
+
 func crawlProducts() {
     urls := getUrls()
     for i := 0; i<len(urls); i++ {
@@ -81,18 +90,20 @@ func crawlProducts() {
             // description := s.Find(".product__description-short").Text()
             // price := s.Find(".product__price").Text()
             // colors := parseColors(s.Find(".shade-picker__color-name"))
-           weight := parseWeight(s.Find(".product__size"))
             
-            fmt.Printf("%s \n", weight)
             //fmt.Printf("%s %s %s %s %s\n", name, description, price, colors, weight)
         })
+        
+        parseHtml(urls[i])
         
         
     }
 }
 
 func main() {
-    fmt.Println("crawling www.mac.com..... \n")
+    
+    os.Mkdir("json",os.ModePerm)
+    
     crawlProducts()
 }
 
